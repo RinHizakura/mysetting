162e7bc6aef9c5c3a943220c72e6d7f26756a6e6
RinHizakura <s921975628@gmail.com>
2025-03-24 18:52:21 +0800

    Add shell configuration in interactive mode only

--- configs/config.fish
+++ configs/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     bass source ~/.nvm/nvm.sh
-end
 
-source "$HOME/.cargo/env.fish"
-set -x NVM_DIR ~/.nvm
+    source "$HOME/.cargo/env.fish"
+    set -x NVM_DIR ~/.nvm
+end
